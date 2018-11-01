6c342c06359e9b462853c1f67ebe9e8b0a25b14f
Oliver Ross <oliver.ross01@bbc.co.uk>
2018-11-01 11:33:36 +0000

    dockerised app

--- app/pages/index.js
+++ app/pages/index.js
@@ -3,7 +3,7 @@ import SVGText from "../components/SVGText";
 
 const Index = props => (
   <Layout colour="#1b1b1b">
-    <SVGText width="100%" height="100%" size="5rem" value="click the button" />
+    <SVGText width="100%" height="100%" size="5rem" value="click the button!" />
   </Layout>
 );
 
