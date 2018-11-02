819390eda415f784498f96625573948761189865
Oliver Ross <oliver.ross01@bbc.co.uk>
2018-11-02 14:17:09 +0000

    show user name

--- app/components/Text.js
+++ app/components/Text.js
@@ -4,6 +4,8 @@ const Text = styled.h1`
   font-family: "Bowlby One SC", cursive;
   color: ${props => props.colour};
   font-size: ${props => props.size};
+  height: ${props => props.size};
+  line-height: ${props => props.size};
   text-align: center;
   overflow-wrap: break-word;
   user-select: none;
--- app/pages/index.js
+++ app/pages/index.js
@@ -2,13 +2,20 @@ import Layout from "../components/Layout";
 import Text from "../components/Text";
 import TodoList from "../components/TodoList";
 
+import styled from "styled-components";
 import fetch from "isomorphic-unfetch";
+
+const Container = styled.div`
+  margin-top: 1rem;
+`;
 
 const Index = props => (
   <Layout colour="#b1b1b1" columns="1fr" rows="10vh 1fr">
-    <Text size="5rem" colour="#4f4f4f">
-      todos
-    </Text>
+    <Container>
+      <Text size="5rem" colour="#4f4f4f">
+        todos for "{props.user}"
+      </Text>
+    </Container>
     <TodoList user={props.user} todos={props.todos} />
   </Layout>
 );
